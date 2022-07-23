43df53db67a9a6a1f830321c540d034628eccdc0
Micah Mills <micahmills@gmail.com>
2022-07-23 20:14:27 +0300

    open-wc init.
    from https://open-wc.org

---
+++ index.js
@@ -0,0 +1,3 @@
+export { DtTextField } from './src/components/dt-text/dt-text.js';
+export { DtSingleSelect } from './src/components/dt-single-select/dt-single-select.js';
+export { DtMultiSelect } from './src/components/dt-multi-select/dt-multi-select.js';
