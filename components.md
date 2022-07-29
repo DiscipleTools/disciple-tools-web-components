3e158678ad5f8b016d57bbb3f93213aa5b53da88
Jon <cairocoder01@gmail.com>
2022-07-29 14:28:42 +0300

    setup rollup to build components

---
+++ components.md
@@ -0,0 +1,31 @@
+DT Components
+
+Fields
+- [ ] Text Field
+- [ ] Boolean
+- [x] [Key select](src/components/dt-single-select/README.md)
+- [x] [Multi Select](src/components/dt-multi-select/README.md)
+- [x] [Tags](src/components/dt-tags/README.md)
+- [ ] Communication Channel
+- [ ] Date
+- [ ] User Select
+- [ ] Number
+- [ ] Location Grid
+- [ ] Location Grid Meta (Mapbox)
+- [ ] Connection
+- [ ] Connection Meta
+- [ ] Post User Meta
+- [ ] Buttons
+
+Page Layout
+- [ ] Header
+- [ ] Second Bar
+- [ ] Post List
+- [ ] Filters accordian
+- [ ] Tile
+- [ ] Comments and Activity
+    - [ ] Comment and Comment Type box
+    - [ ] Quick Actions
+    - [ ] Comments and Activity with Filter
+        - [ ] Google Translate of Comments
+- [ ] Modals
---
+++ rollup.config.js
@@ -0,0 +1,49 @@
+import resolve from '@rollup/plugin-node-resolve';
+import {copy} from '@web/rollup-plugin-copy';
+import { terser } from 'rollup-plugin-terser';
+import minifyHTML from 'rollup-plugin-minify-html-literals';
+import summary from 'rollup-plugin-summary';
+import multiInput from 'rollup-plugin-multi-input';
+
+const copyConfig = {
+  targets: [
+    { src: 'node_modules/@webcomponents', dest: 'build/node_modules' },
+  ]
+};
+
+const config = {
+  input: [
+    './src/components/*/*.js',
+    '!./src/components/*/*.stories.js',
+    '!./src/components/*/*.test.js',
+  ],
+  output: {
+    dir: 'build',
+    format: 'es'
+  },
+  plugins: [
+    multiInput({
+      relative: 'src/components',
+    }),
+    // Resolve bare module specifiers to relative paths
+    resolve(),
+    // Minify HTML template literals
+    minifyHTML(),
+    // Minify JS
+    terser({
+      ecma: 2020,
+      module: true,
+      warnings: true,
+    }),
+    // Print bundle summary
+    summary(),
+    copy(copyConfig),
+  ],
+  preserveEntrySignatures: false
+};
+
+if (process.env.NODE_ENV !== 'development') {
+  config.plugins.push(terser());
+}
+
+export default config;
