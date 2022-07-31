a5d9e33602ef3450cdbe4ae894338e5161e14d24
Micah Mills <micahmills@gmail.com>
2022-08-01 02:55:41 +0300

    added new fields to the kitchen sink

--- src/components/kitchen-sink.stories.js
+++ src/components/kitchen-sink.stories.js
@@ -3,6 +3,8 @@ import { argTypes, themeCss } from '../stories-theme.js';
 import './form/dt-label/dt-label.js';
 import './form/dt-text/dt-text.js';
 import './form/dt-textarea/dt-textarea.js';
+import './form/dt-number/dt-number.js';
+import './form/dt-date/dt-date.js';
 import './form/dt-multi-select/dt-multi-select.js';
 import './form/dt-single-select/dt-single-select.js';
 import './form/dt-tags/dt-tags.js';
@@ -24,21 +26,27 @@ function Template(args) {
     <style>
       ${themeCss(args)}
     </style>
-    
+
     <dt-label>Text Field</dt-label>
-    <dt-text></dt-text>
-    
+    <dt-text id='textField' name='textField' value=''></dt-text>
+
     <dt-label>Textarea Field</dt-label>
-    <dt-textarea></dt-textarea>
+    <dt-textarea id='textareaField' name='textareaField' value=''></dt-textarea>
+
+    <dt-label>Number Field</dt-label>
+    <dt-number></dt-number>
+
+    <dt-label>Date Field</dt-label>
+    <dt-date></dt-date>
 
     <dt-label>Single Select Field</dt-label>
     <dt-single-select placeholder="Select Item"></dt-single-select>
 
     <dt-label>Multi Select Field</dt-label>
     <dt-multi-select placeholder="Select Items" options="${JSON.stringify(tags)}"></dt-multi-select>
-    
+
     <dt-label>Tags Field</dt-label>
-    <dt-tags 
+    <dt-tags
       value="${JSON.stringify([{id:'personal',label:'Personal'}])}"
       options="${JSON.stringify(tags)}"
     ></dt-tags>
