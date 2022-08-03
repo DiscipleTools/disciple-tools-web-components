3567f94d51ad7dc941b99c29030713ea450038d2
Micah Mills <micahmills@gmail.com>
2022-08-03 15:08:23 +0300

    dt-number tests and min max check

--- src/components/form/dt-number/dt-number.stories.js
+++ src/components/form/dt-number/dt-number.stories.js
@@ -20,15 +20,15 @@ export default {
   },
 };
 
-function Template({ id = 'name', name = 'field-name', label = 'Field Name' , value = '', min = '', max = '', disabled = false, icon='https://cdn-icons-png.flaticon.com/512/1077/1077114.png', isPrivate = false, loading = false, saved = false, onchange = 'onChange' }) {
+function Template({ id = 'name', name = 'field-name', label = 'Field Name' , value = '', disabled = false, min = undefined, max = undefined, icon='https://cdn-icons-png.flaticon.com/512/1077/1077114.png', isPrivate = false, loading = false, saved = false, onchange = 'onChange' }) {
   return html`
     <dt-number
       id=${id}
       name=${name}
       label=${label}
-      value=${value}
-      min=${min}
-      max=${max}
+      .value=${value}
+      .min=${min}
+      .max=${max}
       ?disabled=${disabled}
       icon=${icon}
       ?private=${isPrivate}
@@ -44,33 +44,33 @@ export const Empty = Template.bind({});
 
 export const EnteredValue = Template.bind({});
 EnteredValue.args = {
-  value: '7',
+  value: 7,
 };
 
 export const MinValue = Template.bind({});
 MinValue.args = {
-  min: '9',
+  min: 9,
 };
 
 export const MaxValue = Template.bind({});
 MaxValue.args = {
-  max: '10',
+  max: 10,
 };
 
 export const MinMaxValue = Template.bind({});
 MinMaxValue.args = {
-  min: '3',
-  max: '7',
+  min: 3,
+  max: 7,
 };
 
 export const Disabled = Template.bind({});
 Disabled.args = {
   disabled: true,
-  value: '12',
+  value: 12,
 };
 
 export const privateField = Template.bind({});
 privateField.args = {
   isPrivate: true,
-  value: '77',
+  value: 77,
 };
