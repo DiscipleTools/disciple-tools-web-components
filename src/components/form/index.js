c329f5834c92409446b1396ed49af691718fe869
Jon <cairocoder01@gmail.com>
2022-11-29 14:36:49 +0200

    add toggle to form components index

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -1,6 +1,7 @@
 // export { DtSingleSelect } from './dt-communication-channel/dt-communication-channel.js';
 export { DtConnection } from './dt-connection/dt-connection.js';
 export { DtDate } from './dt-date/dt-date.js';
+export { DtLabel } from './dt-label/dt-label.js';
 export { DtLocation } from './dt-location/dt-location.js';
 export { DtMultiSelect } from './dt-multi-select/dt-multi-select.js';
 export { DtNumberField } from './dt-number/dt-number.js';
@@ -8,4 +9,5 @@ export { DtSingleSelect } from './dt-single-select/dt-single-select.js';
 export { DtTags } from './dt-tags/dt-tags.js';
 export { DtText } from './dt-text/dt-text.js';
 export { DtTextArea } from './dt-textarea/dt-textarea.js';
+export { DtToggle } from './dt-toggle/dt-toggle.js';
 // export { DtUserSelect } from './dt-user-select/dt-user-select.js';
