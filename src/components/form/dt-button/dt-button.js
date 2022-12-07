2727a43ec351c04e30fac3d9206499affac30ea2
Micah Mills <micahmills@gmail.com>
2022-12-07 10:37:16 -0500

    Added button component from CodeZone Autolink

---
+++ src/components/form/dt-button/dt-button.js
@@ -0,0 +1,232 @@
+import { html, css } from "lit";
+import { classMap } from "lit/directives/class-map.js";
+import DtBase from '../../dt-base.js';
+
+export class DtButton extends DtBase {
+  static get styles() {
+    return css`
+      :host {
+        display: inline-flex;
+        width: fit-content;
+        height: fit-content;
+      }
+
+      .dt-button {
+        cursor: pointer;
+        display: flex;
+        padding: var(--dt-button-padding-y, 10px)
+          var(--dt-button-padding-x, 10px);
+        font-family: var(--dt-button-font-family);
+        font-size: var(--dt-button-font-size, 14px);
+        line-height: var(--dt-button-line-height, inherit);
+        font-weight: var(--dt-button-font-weight, 700);
+        background-color: var(
+          --dt-button-context-background-color,
+          var(--dt-button-background-color)
+        );
+        border: var(--dt-button-border-width, 1px) solid
+          var(--dt-button-context-border-color, var(--dt-button-border-color));
+        border-radius: var(--dt-button-border-radius, 10px);
+        box-shadow: var(
+          --dt-button-box-shadow,
+          --dt-button-context-box-shadow(0 2px 4px rgb(0 0 0 / 25%))
+        );
+        color: var(--dt-button-context-text-color, var(--dt-button-text-color));
+        text-rendering: optimizeLegibility;
+        gap: 10px;
+        justify-content: center;
+        align-content: center;
+        align-items: center;
+        text-decoration: var(
+          --dt-button-text-decoration,
+          var(--dt-button-context-text-decoration, none)
+        );
+        text-transform: var(--dt-button-text-transform, none);
+        letter-spacing: var(--dt-button-letter-spacing, normal);
+        width: var(--dt-button-width, 100%);
+        height: var(--dt-button-height, auto);
+        aspect-ratio: var(--dt-button-aspect-ratio, auto);
+        position: relative;
+      }
+
+      .dt-button.dt-button--outline {
+        background-color: transparent;
+        color: var(--dt-button-context-text-color, var(--text-color-inverse));
+      }
+
+      .dt-button--primary:not(.dt-button--outline) {
+        --dt-button-context-border-color: var(--primary-color);
+        --dt-button-context-background-color: var(--primary-color);
+        --dt-button-context-text-color: var(--dt-button-text-color-light);
+      }
+
+      .dt-button--link:not(.dt-button--outline) {
+        --dt-button-context-text-decoration: underline;
+        --dt-button-context-box-shadow: none;
+        --dt-button-context-border-color: transparent;
+        --dt-button-context-background-color: transparent;
+        --dt-button-context-text-color: var(--dt-button-text-color-dark);
+      }
+
+      .dt-button--alert:not(.dt-button--outline) {
+        --dt-button-context-border-color: var(--alert-color);
+        --dt-button-context-background-color: var(--alert-color);
+        --dt-button-context-text-color: var(--dt-button-text-color-light);
+      }
+
+      .dt-button--caution:not(.dt-button--outline) {
+        --dt-button-context-border-color: var(--caution-color);
+        --dt-button-context-background-color: var(--caution-color);
+        --dt-button-context-text-color: var(--dt-button-text-color-dark);
+      }
+
+      .dt-button--success:not(.dt-button--outline) {
+        --dt-button-context-border-color: var(--success-color);
+        --dt-button-context-background-color: var(--success-color);
+        --dt-button-context-text-color: var(--dt-button-text-color-light);
+      }
+
+      .dt-button--inactive:not(.dt-button--outline) {
+        --dt-button-context-border-color: var(--inactive-color);
+        --dt-button-context-background-color: var(--inactive-color);
+        --dt-button-context-text-color: var(--dt-button-text-color-light);
+      }
+
+      .dt-button--disabled:not(.dt-button--outline) {
+        --dt-button-context-border-color: var(--disabled-color);
+        --dt-button-context-background-color: var(--disabled-color);
+        --dt-button-context-text-color: var(--dt-button-text-color-dark);
+      }
+
+      .dt-button--primary.dt-button--outline {
+        --dt-button-context-border-color: var(--primary-color);
+        --dt-button-context-text-color: var(--primary-color);
+      }
+
+      .dt-button--alert.dt-button--outline {
+        --dt-button-context-border-color: var(--alert-color);
+        --dt-button-context-text-color: var(--alert-color);
+      }
+
+      .dt-button--caution.dt-button--outline {
+        --dt-button-context-border-color: var(--caution-color);
+        --dt-button-context-text-color: var(--caution-color);
+      }
+
+      .dt-button--success.dt-button--outline {
+        --dt-button-context-border-color: var(--success-color);
+        --dt-button-context-text-color: var(--success-color);
+      }
+
+      .dt-button--inactive.dt-button--outline {
+        --dt-button-context-border-color: var(--inactive-color);
+      }
+
+      .dt-button--disabled.dt-button--outline {
+        --dt-button-context-border-color: var(--disabled-color);
+      }
+
+      .dt-button.dt-button--rounded {
+        --dt-button-border-radius: 50%;
+        --dt-button-padding-x: 0px;
+        --dt-button-padding-y: 0px;
+        --dt-button-width: var(--dt-button-width, 1.2em);
+        --dt-button-aspect-ratio: var(--dt-button-aspect-ratio, 1/1);
+        --dt-button-line-height: var(--dt-button-height, inherit);
+      }
+
+      button.toggle {
+        margin-inline-end: 0;
+        margin-inline-start: auto;
+        background: none;
+        border: none;
+        color: inherit;
+        cursor: pointer;
+        display: flex;
+        align-items: center;
+      }
+    `;
+  }
+
+  static get properties() {
+    return {
+      context: { type: String },
+      type: { type: String },
+      outline: { type: Boolean },
+      href: { type: String },
+      title: { type: String },
+      onClick: { type: Function },
+      rounded: { type: Boolean },
+    };
+  }
+
+  get classes() {
+    const classes = {
+      "dt-button": true,
+      "dt-button--outline": this.outline,
+      "dt-button--rounded": this.rounded,
+    };
+    const contextClass = `dt-button--${this.context}`;
+    classes[contextClass] = true;
+    return classes;
+  }
+
+  constructor() {
+    super();
+
+    this.context = "default";
+  }
+
+  handleClick(e) {
+    if (this.onClick) {
+      e.preventDefault();
+      this.onClick(e);
+    } else {
+      const form = this.closest("form");
+      if (form) {
+        form.submit();
+      }
+    }
+  }
+
+  _dismiss() {
+    this.hide = true;
+  }
+
+  render() {
+    if (this.hide) {
+      return html``;
+    }
+
+    if (this.href) {
+      return html`
+        <a
+          class=${classMap(this.classes)}
+          href=${this.href}
+          title=${this.title}
+          type=${this.type}
+          @click=${() => this.handleClick()}
+        >
+          <div>
+            <slot></slot>
+          </div>
+        </a>
+      `;
+    }
+
+    return html`
+      <button
+        class=${classMap(this.classes)}
+        title=${this.title}
+        type=${this.type}
+        @click=${() => this.handleClick()}
+      >
+        <div>
+          <slot></slot>
+        </div>
+      </button>
+    `;
+  }
+}
+
+window.customElements.define("dt-button", DtButton);
---
+++ src/components/form/dt-button/dt-button.stories.js
@@ -0,0 +1,120 @@
+import { html } from 'lit-html';
+import {
+  themes,
+  themeCss,
+  argTypes,
+  buttonContexts,
+} from '../../../stories-theme.js';
+import { LocaleDecorator } from '../../../stories-utils.js';
+import './dt-button.js';
+
+export default {
+  title: 'Form/dt-button',
+  component: 'dt-button',
+  argTypes: {
+    theme: {
+      control: 'select',
+      options: Object.keys(themes),
+      defaultValue: 'default',
+    },
+    context: {
+      control: 'select',
+      options: ['primary', ...buttonContexts],
+      defaultValue: 'primary',
+    },
+    ...argTypes,
+  },
+};
+
+const Template = args => {
+  const { slot = 'Button' } = args;
+  return html`
+    <style>
+      ${themeCss(args)}
+    </style>
+    <dt-button
+      .context="${args.context}"
+      type="${args.type}"
+      ?outline="${args.outline}"
+      ?rounded="${args.rounded}"
+      .href="${args.href}"
+      title="${args.title}"
+    >
+      ${slot}
+    </dt-button>
+  `;
+};
+export const Default = Template.bind({});
+Default.args = {
+  context: 'primary',
+};
+
+export const Link = Template.bind({});
+Link.args = {
+  context: 'link',
+};
+
+export const Alert = Template.bind({});
+Alert.args = {
+  context: 'alert',
+};
+
+export const Caution = Template.bind({});
+Caution.args = {
+  context: 'caution',
+};
+export const Success = Template.bind({});
+Success.args = {
+  context: 'success',
+};
+export const Inactive = Template.bind({});
+Inactive.args = {
+  context: 'inactive',
+};
+export const Disabled = Template.bind({});
+Disabled.args = {
+  context: 'disabled',
+};
+export const OutlineDefault = Template.bind({});
+OutlineDefault.args = {
+  outline: true,
+  context: 'primary',
+};
+
+export const OutlineLink = Template.bind({});
+OutlineLink.args = {
+  outline: true,
+  context: 'link',
+};
+
+export const OutlineAlert = Template.bind({});
+OutlineAlert.args = {
+  outline: true,
+  context: 'alert',
+};
+
+export const OutlineCaution = Template.bind({});
+OutlineCaution.args = {
+  outline: true,
+  context: 'caution',
+};
+export const OutlineSuccess = Template.bind({});
+OutlineSuccess.args = {
+  outline: true,
+  context: 'success',
+};
+export const OutlineInactive = Template.bind({});
+OutlineInactive.args = {
+  outline: true,
+  context: 'inactive',
+};
+export const OutlineDisabled = Template.bind({});
+OutlineDisabled.args = {
+  outline: true,
+  context: 'disabled',
+};
+
+export const RoundedDefault = Template.bind({});
+RoundedDefault.args = {
+  rounded: true,
+};
