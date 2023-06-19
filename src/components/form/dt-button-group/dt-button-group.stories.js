0b79cab13f15a3bab9f4777b9b5d8df1d645298e
corsacca <corsacca@gmail.com>
2023-06-19 15:33:48 +0200

    init multi-select button group

---
+++ src/components/form/dt-button-group/dt-button-group.stories.js
@@ -0,0 +1,230 @@
+import { html } from 'lit';
+import { themes, themeCss, argTypes } from '../../../stories-theme.js';
+import { LocaleDecorator, FormDecorator } from '../../../stories-utils.js';
+import './dt-button-group.js';
+
+const basicOptions = [
+  {
+    id: 'opt1',
+    label: 'Option 1',
+  },
+  {
+    id: 'opt2',
+    label: 'Option 2',
+  },
+  {
+    id: 'opt3',
+    label: 'Option 3',
+  },
+  {
+    id: 'opt4',
+    label: 'Option 4',
+  },
+  {
+    id: 'opt5',
+    label: 'Option 5',
+  },
+  {
+    id: 'opt6',
+    label: 'Option 6',
+  },
+  {
+    id: 'opt7',
+    label: 'Option 7',
+  },
+  {
+    id: 'opt8',
+    label: 'Option 8',
+  },
+];
+export default {
+  title: 'Form/dt-button-group',
+  component: 'dt-button-group',
+  argTypes: {
+    theme: {
+      control: 'select',
+      options: Object.keys(themes),
+      defaultValue: 'default',
+    },
+    name: {
+      control: 'text',
+      type: { name: 'string', required: true },
+      description:
+        'Passed to `change` function to identify which input triggered the event',
+    },
+    value: {
+      control: 'text',
+      type: { name: 'array' },
+      table: {
+        type: {
+          summary: 'string[]',
+          detail: `['1', '345', '83']`,
+        },
+      },
+      description:
+        'Array of values indicating the selected values. Should be an array of strings converted to a string with `JSON.stringify`. <br/>**Note:** This attribute will be updated on the HTML element when value changes.',
+    },
+    options: {
+      description:
+        'Array of available options to choose.' +
+        '<br/>**Format:** Array of objects with keys `id` and `label`. Convert to string with `JSON.stringify`. ',
+      table: {
+        type: {
+          summary: '{id:string, label:string}[]',
+          detail: `[{id:'1',label:'Item 1'},{id:'345',label:'Item 345'}]`,
+        },
+      },
+    },
+    loading: {
+      control: 'boolean',
+      description:
+        '(true|false) If attribute is present, the loading spinner will be displayed within the field',
+      table: {
+        type: {
+          summary: 'loading',
+          detail: '<dt-button-group loading />',
+        },
+      },
+    },
+    saved: {
+      control: 'boolean',
+      description:
+        '(true|false) If attribute is present, the saved checkmark will be displayed within the field',
+      table: {
+        type: {
+          summary: 'saved',
+          detail: '<dt-button-group saved />',
+        },
+      },
+    },
+    onchange: {
+      control: 'text',
+      description:
+        'Javascript code to be executed when the value of the field changes. Makes available a `event` variable that includes field name, old value, and new value in `event.details`',
+      table: {
+        type: {
+          summary: 'onChange(event)',
+          detail: '<dt-button-group onchange="onChange(event)" />',
+        },
+      },
+    },
+    locale: {
+      control: 'text',
+    },
+    ...argTypes,
+  },
+};
+
+function Template(args) {
+  const {
+    name = 'field-name',
+    label = 'Field Name',
+    options,
+    value,
+    disabled = false,
+    icon = 'https://cdn-icons-png.flaticon.com/512/1077/1077114.png',
+    iconAltText = 'Icon Alt Text',
+    isPrivate,
+    privateLabel,
+    loading = false,
+    saved = false,
+    onchange,
+    slot,
+  } = args;
+  return html`
+    <style>
+      ${themeCss(args)}
+    </style>
+    <dt-button-group
+      name="${name}"
+      label=${label}
+      options="${JSON.stringify(options)}"
+      value="${JSON.stringify(value)}"
+      onchange="${onchange}"
+      ?disabled=${disabled}
+      icon="${icon}"
+      iconAltText="${iconAltText}"
+      ?private=${isPrivate}
+      privateLabel="${privateLabel}"
+      ?loading="${loading}"
+      ?saved="${saved}"
+    >
+      ${slot}
+    </dt-button-group>
+  `;
+}
+
+export const Empty = Template.bind({});
+
+export const SvgIcon = Template.bind({});
+SvgIcon.args = {
+  icon: null,
+  // prettier-ignore
+  slot: html`<svg slot="icon-start" xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 100 100"><linearGradient id="lg"><stop offset="0%" stop-color="#000000"/><stop offset="100%" stop-color="#c3c3c3"/></linearGradient><rect x="2" y="2" width="96" height="96" style="fill:url(#lg);stroke:#ffffff;stroke-width:2"/><text x="50%" y="50%" font-size="18" text-anchor="middle" alignment-baseline="middle" font-family="monospace, sans-serif" fill="#ffffff">icon</text></svg>`,
+};
+
+export const CustomOptions = Template.bind({});
+CustomOptions.args = {
+  options: basicOptions,
+};
+
+export const SelectedValue = Template.bind({});
+SelectedValue.args = {
+  value: ['opt2', 'opt3'],
+  options: basicOptions,
+};
+
+export const AutoSave = Template.bind({});
+AutoSave.args = {
+  options: basicOptions,
+  onchange: 'onAutoSave(event)',
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  value: ['opt2'],
+  options: basicOptions,
+  disabled: true,
+};
+export const Loading = Template.bind({});
+Loading.args = {
+  value: ['opt2'],
+  options: basicOptions,
+  loading: true,
+};
+export const Saved = Template.bind({});
+Saved.args = {
+  value: ['opt2'],
+  options: basicOptions,
+  saved: true,
+};
+
+export const BasicForm = Template.bind({});
+BasicForm.decorators = [LocaleDecorator, FormDecorator];
+BasicForm.args = {
+  value: ['opt2'],
+  options: basicOptions,
+};
+
+export const LocalizeRTL = Template.bind({});
+LocalizeRTL.decorators = [LocaleDecorator];
+LocalizeRTL.args = {
+  lang: 'ar',
+  dir: 'rtl',
+  label: 'اسم الإدخال',
+  value: ['opt1'],
+  options: [
+    {
+      id: 'opt1',
+      label: 'تنكر هؤلاء الرجال المفتونون',
+    },
+    {
+      id: 'opt2',
+      label: 'م فيتساوي مع هؤلاء',
+    },
+    {
+      id: 'opt3',
+      label: 'فلا أحد يرفض',
+    },
+  ],
+};
---
+++ src/components/form/dt-button-group/dt-button-group.test.js
@@ -0,0 +1,55 @@
+import { html } from 'lit';
+import { fixture, expect, oneEvent } from '@open-wc/testing';
+import { sendKeys } from '@web/test-runner-commands';
+
+import './dt-button-group.js';
+
+const options = [
+  {
+    id: 'opt1',
+    label: 'Option 1',
+  },
+  {
+    id: 'opt2',
+    label: 'Second Option',
+  },
+  {
+    id: 'opt3',
+    label: 'Option Three',
+  },
+];
+async function wait(ms) {
+  return new Promise(r => {
+    setTimeout(r, ms);
+  });
+}
+
+async function clickOption(el, id) {
+  const optionBtn = el.shadowRoot.querySelector(
+    `.button[value=${id}]`
+  );
+
+  await wait(50); // wait for UI update
+
+  optionBtn.click();
+  await wait(100);
+}
+
+describe('dt-button-group', () => {
+
+  it('sets options', async () => {
+    const el = await fixture(
+      html`<dt-button-group
+        options="${JSON.stringify(options)}"
+      ></dt-button-group>`
+    );
+    const optionList = el.shadowRoot.querySelector('.button-group');
+
+    expect(optionList.children).to.have.lengthOf(3);
+    expect(optionList).to.contain('button[value=opt1]');
+    expect(optionList).to.contain('button[value=opt2]');
+    expect(optionList).to.contain('button[value=opt3]');
+
+    expect(optionList).not.to.be.displayed;
+  });
+});
