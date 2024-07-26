ad3f1b65cbe61a08c2b4e1db7fa22e9cbb8c6b01
Micah Mills <micahmills@gmail.com>
2024-07-27 01:48:22 +0300

    added RTL story to test RTL languages

--- src/components/form/dt-dropdown/dt-dropdown.stories.js
+++ src/components/form/dt-dropdown/dt-dropdown.stories.js
@@ -36,7 +36,9 @@ const Template = args => {
   const {options,buttonStyle} = args;
 
   return html`
-
+    <style>
+      ${themeCss(args)}
+    </style>
      <dt-dropdown label=${args.label} options=${JSON.stringify(options)} selectedOptionLabel=${args.selectedOptionLabel} buttonStyle=${JSON.stringify(buttonStyle)}>
       </dt-dropdown>
 
@@ -53,6 +55,27 @@ Default.args = {
     { label: 'Option 3', icon: '/assets/trash.svg', isModal: false, href: 'https://www.google.com' },
   ],
   selectedOptionLabel:'Action Items',
+  buttonStyle: {
+    padding: '8px',
+    backgroundColor: '#00897B',
+    color: 'white',
+    borderRadius: '4px',
+   border:'none',
+  },
+};
+
+export const LocalizeRTL = Template.bind({});
+LocalizeRTL.decorators = [LocaleDecorator];
+LocalizeRTL.args = {
+  lang: 'ar',
+  dir: 'rtl',
+  label: 'خطوات العمل',
+  options:[
+    { label: 'الخيار 1', icon: '/assets/circle-square-triangle.svg?v=2', isModal: true },
+    { label: 'الخيار 2', icon: '/assets/arrow-user.svg', isModal: true },
+    { label: 'الخيار 3', icon: '/assets/trash.svg', isModal: false, href: 'https://www.google.com' },
+  ],
+  selectedOptionLabel:'خطوات العمل',
   buttonStyle: {
     padding: '8px',
     backgroundColor: '#00897B',
