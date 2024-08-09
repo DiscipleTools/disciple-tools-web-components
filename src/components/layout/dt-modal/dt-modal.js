837010f9b0ceeb4a0064ba5431311cbb03b43176
Vijenderrr <vsingh@covalience.com>
2024-08-09 15:13:34 +0530

    design fixes for duplicate-detected button template integration

--- src/components/layout/dt-modal/dt-modal.js
+++ src/components/layout/dt-modal/dt-modal.js
@@ -155,6 +155,33 @@ export class DtModal extends DtBase {
         filter: invert(69%) sepia(1%) saturate(0) hue-rotate(239deg) brightness(94%) contrast(86%);
         height: 15px;
       }
+        .dt-modal.header-blue-bg {
+          padding: 0; 
+        }
+        .dt-modal.header-blue-bg header {   
+          background-color: #3f729b;
+          color: #fff;
+          text-align: center;
+          padding-top: 10px;
+        }
+        .dt-modal.header-blue-bg header #modal-field-title {   
+          font-size: 1.5rem;
+          width: 100%;
+        }
+        .dt-modal.header-blue-bg article {
+          padding: 10px 0;
+        }
+        .dt-modal.header-blue-bg footer {
+          padding-left: .7rem;
+          padding-right: .7rem;
+          justify-content: flex-end;
+        }
+        .dt-modal.header-blue-bg footer .button {
+          padding: 12px 14px;
+        }
+        .dt-modal.header-blue-bg form {
+          grid-template-rows: 52px auto 85px;
+              }
     `;
   }
 
@@ -168,10 +195,11 @@ export class DtModal extends DtBase {
       hideButton: { type: Boolean },
       buttonClass: { type: Object },
       buttonStyle: { type: Object },
-      imageSrc: {type: String},
-      imageStyle: {type:Object},
-      tileLabel: {type:String},
-      buttonLabel:{type: String},
+      headerClass: { type: Object },
+      imageSrc: { type: String },
+      imageStyle: { type: Object },
+      tileLabel: { type: String },
+      buttonLabel: { type: String },
     };
   }
 
@@ -188,7 +216,7 @@ export class DtModal extends DtBase {
 
     document.querySelector('body').style.overflow = "hidden"
   }
-// to format title coming from backend
+  // to format title coming from backend
 
   get formattedTitle() {
     if (!this.title) return '';
@@ -296,7 +324,7 @@ export class DtModal extends DtBase {
     return html`
       <dialog
         id=""
-        class="dt-modal dt-modal--width"
+        class="dt-modal dt-modal--width ${classMap(this.headerClass || {})}"
         @click=${this._dialogClick}
         @keypress=${this._dialogKeypress}
       >
@@ -321,7 +349,7 @@ export class DtModal extends DtBase {
       </dialog>
 
       ${!this.hideButton
-      ? html`
+        ? html`
       <button
         class="button small opener ${classMap(this.buttonClass || {})}"
         data-open=""
@@ -331,15 +359,15 @@ export class DtModal extends DtBase {
         style=${styleMap(this.buttonStyle || {})}
       >
       ${this.imageSrc
-               ? html`<img
+            ? html`<img
                    src="${this.imageSrc}"
                    alt="${this.buttonLabel} icon"
                    class="help-icon"
                    style=${styleMap(this.imageStyle || {})}
                  />`
-               : ''}
+            : ''}
       ${this.buttonLabel
-      ?html`${this.buttonLabel}`:''}
+            ? html`${this.buttonLabel}` : ''}
       </button>
       ` : null}
     `;
