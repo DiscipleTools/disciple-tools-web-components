2e0be8a48810b38adc1da03309f130cbe82ec103
Brady Lamansky <brady.l@global-gtt.com>
2025-10-01 14:12:33 -0400

    removed unneeded line

--- src/components/form/dt-location/dt-location.js
+++ src/components/form/dt-location/dt-location.js
@@ -121,8 +121,6 @@ export class DtLocation extends DtTags {
     if (!currentValue && select?.clientWidth > 0) {
       container.style.setProperty('--select-width', `${select.clientWidth}px`);
     }
-
-    this._updateContainerHeight();
   }
 
   /**
