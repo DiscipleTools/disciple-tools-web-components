6375e74b73587b04635575dbde5fbd6cc8a1d37c
Brady Lamansky <brady.l@global-gtt.com>
2026-01-27 16:36:51 -0500

    dt-multi-text-link component functionality (early version)

---
+++ src/components/form/dt-multi-text-link/dt-multi-text-link.stories.js
@@ -0,0 +1,228 @@
+import { html } from 'lit';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { action } from '@storybook/addon-actions';
+import { argTypes } from '../../../stories-theme.js';
+import {
+  FormDecorator,
+  LocaleDecorator,
+  onAutoSave,
+} from '../../../stories-utils.js';
+import './dt-multi-text-link.js';
+
+export default {
+  title: 'Components/Form/Text - Multi Link',
+  component: 'dt-multi-text-link',
+  argTypes: {
+    name: { control: 'text' },
+    label: { control: 'text' },
+    value: {
+      control: 'text',
+      type: { name: 'array' },
+    },
+    placeholder: { control: 'text' },
+    type: {
+      control: 'select',
+      options: ['text', 'password', 'email', 'number', 'tel', 'url'],
+      defaultValue: 'text',
+    },
+    disabled: { control: 'boolean' },
+    loading: { control: 'boolean' },
+    saved: { control: 'boolean' },
+    groups: {
+      control: 'text',
+      type: { name: 'array' },
+    },
+    ...argTypes,
+  },
+  args: {
+    onChange: action('on-change'),
+  },
+};
+
+function Template(args) {
+  const {
+    name = 'field-name',
+    label = 'Field Name',
+    value = '',
+    placeholder,
+    disabled = false,
+    required = false,
+    requiredMessage,
+    icon = 'https://cdn-icons-png.flaticon.com/512/1077/1077114.png',
+    iconAltText = 'Icon Alt Text',
+    isPrivate,
+    privateLabel,
+    loading = false,
+    saved = false,
+    groups = '',
+    error,
+    onChange,
+    slot,
+    type,
+  } = args;
+  return html`
+    <dt-multi-text-link
+      name=${name}
+      label=${label}
+      .value=${value}
+      placeholder=${placeholder}
+      type=${type}
+      ?disabled=${disabled}
+      ?required=${required}
+      requiredMessage=${requiredMessage}
+      icon="${icon}"
+      iconAltText="${iconAltText}"
+      ?private=${isPrivate}
+      privateLabel="${privateLabel}"
+      ?loading=${loading}
+      ?saved=${saved}
+      .groups=${groups}
+      error="${ifDefined(error)}"
+      @change=${onChange}
+    >
+      ${slot}
+    </dt-multi-text-link>
+  `;
+}
+
+export const Empty = Template.bind({});
+
+export const SvgIcon = Template.bind({});
+SvgIcon.args = {
+  icon: null,
+  slot: 'SvgIcon',
+};
+
+export const CustomPlaceholder = Template.bind({});
+CustomPlaceholder.args = {
+  placeholder: 'Enter a value',
+};
+
+export const EnteredValue = Template.bind({});
+EnteredValue.args = {
+  value: [
+    {
+      verified: false,
+      value: 'test1',
+      key: 'comm_channel_1',
+    },
+    {
+      verified: false,
+      value: 'test2',
+      key: 'comm_channel_2',
+    },
+  ],
+};
+
+export const AutoSave = Template.bind({});
+AutoSave.args = {
+  onChange: onAutoSave,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+  value: [
+    {
+      value: 'Lorem Ipsum',
+      key: 'comm_channel_1',
+    },
+  ],
+};
+
+export const privateField = Template.bind({});
+privateField.args = {
+  isPrivate: true,
+  value: [
+    {
+      value: 'Lorem Ipsum',
+      key: 'comm_channel_1',
+    },
+  ],
+  privateLabel: 'This is a custom tooltip',
+};
+
+export const Loading = Template.bind({});
+Loading.args = {
+  loading: true,
+};
+export const Saved = Template.bind({});
+Saved.args = {
+  saved: true,
+};
+export const Groups = Template.bind({});
+Groups.args = {
+  groups: [
+    { id: 'default', label: 'Default' }, 
+    { id: 'one', label: 'Group 1' }
+  ],
+  value: [
+    {
+      value: 'Lorem Ipsum',
+      key: 'comm_channel_1',
+      group: 'default',
+    },
+    {
+      value: 'Lorem Ipsum 2',
+      key: 'comm_channel_2',
+      group: 'one',
+    },
+    {
+      value: 'Lorem Ipsum 3',
+      key: 'comm_channel_3',
+      group: 'one',
+    },
+  ],
+};
+export const Error = Template.bind({});
+Error.args = {
+  error: 'Custom error message',
+};
+
+export const ErrorSlot = Template.bind({});
+ErrorSlot.args = {
+  error: '[Should show link here]',
+  slot: 'ErrorSlot',
+};
+
+export const BasicForm = Template.bind({});
+BasicForm.decorators = [FormDecorator];
+BasicForm.args = {
+  value: [
+    {
+      value: 'Lorem Ipsum',
+      key: 'comm_channel_1',
+    },
+  ],
+};
+
+export const Required = Template.bind({});
+Required.decorators = [FormDecorator];
+Required.args = {
+  required: true,
+};
+
+export const password = Template.bind({});
+password.args = {
+  type: 'password',
+};
+
+export const requiredCustomMessage = Template.bind({});
+requiredCustomMessage.args = {
+  required: true,
+  requiredMessage: 'Custom error message',
+};
+
+export const LocalizeRTL = Template.bind({});
+LocalizeRTL.decorators = [LocaleDecorator, FormDecorator];
+LocalizeRTL.args = {
+  lang: 'ar',
+  dir: 'rtl',
+  label: 'اسم الإدخال',
+  value: [
+    {
+      value: 'راد أن يشع',
+      key: 'comm_channel_1',
+    },
+  ],
+};
