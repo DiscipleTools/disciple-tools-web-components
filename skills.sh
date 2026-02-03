4d7dbbf11fe94ec29457522ef4ef77ae81560bc2
Jon <cairocoder01@gmail.com>
2026-02-03 10:43:05 +0200

    fix default SKILLS_AGENTS replacement

--- skills.sh
+++ skills.sh
@@ -2,7 +2,9 @@
 # Run skills with the agents from the environment variable or defaults
 # Needs to run in this bash script so environment variables from dotenvx can be picked up (they don't get properly injected into npm script)
 #echo $SKILLS_AGENTS
+: ${SKILLS_AGENTS:="claude-code cursor github-copilot gemini-cli"}
+#echo $SKILLS_AGENTS
 
 # -y Yes to all prompts
 # -a specify agents - space-delimited
-npx skills add ./skills -y -a ${SKILLS_AGENTS:-"claude-code cursor github-copilot gemini-cli"}
+npx skills add ./skills -y -a $SKILLS_AGENTS
