0ebe161dc4ee9325d16561603428266ac736edff
Jon <cairocoder01@gmail.com>
2026-02-09 15:59:03 +0200

    move styles to style.css for better theme support

--- .storybook/themeDecorator.js
+++ .storybook/themeDecorator.js
@@ -6,38 +6,5 @@ const { initializeThemeState, pluckThemeFromContext } = DecoratorHelpers;
 
 const DEFAULT_ELEMENT_SELECTOR = 'html';
 
-const classStringToArray = (classString) => classString.split(' ').filter(Boolean);
-
-export const withCssFileTheme = ({ themes, defaultTheme, parentSelector = DEFAULT_ELEMENT_SELECTOR }) => {
-  initializeThemeState(Object.keys(themes), defaultTheme);
-
-  return (storyFn, context) => {
-    const { themeOverride } = context.parameters.themes ?? {};
-    const selected = pluckThemeFromContext(context);
-
-    useEffect(() => {
-      const selectedThemeName = themeOverride || selected || defaultTheme;
-      const head = document.querySelector('head');
-
-      if (!head) {
-        return;
-      }
-
-      let linkExists = false
-      head.querySelectorAll('link[data-theme]').forEach((el) => {
-        if (!linkExists && el.dataset.theme === selectedThemeName) {
-          linkExists = true;
-        } else {
-          el.remove();
-        }
-      });
-
-      if (!linkExists) {
-        const link = `<link href="/${themes[selectedThemeName]}.css" type="text/css" rel="stylesheet" data-theme="${selectedThemeName}">`
-        document.head.innerHTML += link;
-      }
-    }, [themeOverride, selected, parentSelector]);
-
-    return storyFn();
-  };
-};
+const classStringToArray = classString =>
+  classString.split(' ').filter(Boolean);
