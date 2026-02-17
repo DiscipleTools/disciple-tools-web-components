e1a240517ceec69a023731865a362b80c903c9d2
Brady Lamansky <brady.l@global-gtt.com>
2026-02-17 16:38:21 -0500

    added multi-text-groups export for the theme

--- src/components/form/index.js
+++ src/components/form/index.js
@@ -15,4 +15,5 @@ export { DtTextArea } from './dt-textarea/dt-textarea.js';
 export { DtToggle } from './dt-toggle/dt-toggle.js';
 export { DtMultiText } from './dt-multi-text/dt-multi-text.js';
 export { DtMultiSelectButtonGroup } from './dt-multi-select-button-group/dt-multi-select-button-group.js';
+export { DtMultiTextGroups } from './dt-multi-text-groups/dt-multi-text-groups.js';
 // export { DtUserSelect } from './dt-user-select/dt-user-select.js';
